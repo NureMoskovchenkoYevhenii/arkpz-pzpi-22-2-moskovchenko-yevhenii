e3ddae3510eed0964fa851cd7b45f9644deccbeb
NureMoskovchenkoYevhenii <115986310+NureMoskovchenkoYevhenii@users.noreply.github.com>
2025-01-09 15:41:09 +0200

    Add files via upload

---
+++ Task3/arkpz-pzpi-22-2-moskovchenko-yevhenii-task3/Repositories/SensorDataRepository.cs
@@ -0,0 +1,65 @@
+﻿public interface ISensorDataRepository
+{
+    void Add(SensorData sensorData);
+    IEnumerable<SensorData> GetAll();
+    SensorData GetById(int id);
+    void Update(SensorData sensorData);
+    void Delete(int id);
+    SensorData GetLastSensorData(); 
+
+}
+
+public class SensorDataRepository : ISensorDataRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public SensorDataRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(SensorData sensorData)
+    {
+        _context.SensorData.Add(sensorData);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<SensorData> GetAll()
+    {
+        return _context.SensorData.ToList();
+    }
+
+    public SensorData GetById(int id)
+    {
+        return _context.SensorData.Find(id);
+    }
+
+    public void Update(SensorData sensorData)
+    {
+        var existingSensorData = _context.SensorData.Find(sensorData.Id);
+        if (existingSensorData != null)
+        {
+            existingSensorData.Timestamp = sensorData.Timestamp;
+            existingSensorData.Temperature = sensorData.Temperature;
+            existingSensorData.Humidity = sensorData.Humidity;
+            existingSensorData.IsTemperatureAdjustmentEnabled = sensorData.IsTemperatureAdjustmentEnabled;
+            existingSensorData.IsHumidityAdjustmentEnabled = sensorData.IsHumidityAdjustmentEnabled;
+            _context.SaveChanges();
+        }
+    }
+
+    public void Delete(int id)
+    {
+        var sensorData = _context.SensorData.Find(id);
+        if (sensorData != null)
+        {
+            _context.SensorData.Remove(sensorData);
+            _context.SaveChanges();
+        }
+    }
+
+    public SensorData GetLastSensorData()
+    {
+        return _context.SensorData.OrderByDescending(s => s.Timestamp).FirstOrDefault();
+    }
+}
