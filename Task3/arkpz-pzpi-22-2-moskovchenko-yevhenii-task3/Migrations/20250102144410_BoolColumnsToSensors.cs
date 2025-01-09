be85a6a3973932bd9a5effd05bc9b8c63b0516de
NureMoskovchenkoYevhenii <115986310+NureMoskovchenkoYevhenii@users.noreply.github.com>
2025-01-09 15:40:09 +0200

    Add files via upload

---
+++ Task3/arkpz-pzpi-22-2-moskovchenko-yevhenii-task3/Migrations/20250102144410_BoolColumnsToSensors.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StaffFlow_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class BoolColumnsToSensors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "tmperature",
+                table: "SensorData",
+                newName: "temperature");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "temperature",
+                table: "SensorData",
+                newName: "tmperature");
+        }
+    }
+}
